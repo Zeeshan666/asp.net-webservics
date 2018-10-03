d216ba4cd7ce98e290317ec8648d3c11392a017c
Zeeshan Manzoor <zeeshanmanzoor.zm@gmal.com>
2018-10-03 14:10:15 +0500

    first commit

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApplication1.ServiceReference1;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        WebServiceSoapClient obj;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            obj= new WebServiceSoapClient();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int ans = obj.add(a, b);
+            MessageBox.Show("addition is "+ans);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int ans = obj.sub(a, b);
+            MessageBox.Show("subtraction is " + ans);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int ans = obj.mul(a, b);
+            MessageBox.Show("multiplication is " + ans);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            int ans = obj.divide(a, b);
+            MessageBox.Show("divison is " + ans);
+        }
+    }
+}
